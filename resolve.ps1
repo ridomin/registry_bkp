c8519fa2f1dbb5e83de8a602cf2b9ec9cde5c8f0
ridomin <ridomin@live.com>
2020-09-04 08:55:24 -0700

    resolve script

--- resolve.ps1
+++ resolve.ps1
@@ -1,11 +1,11 @@
 $repo="https://iotmodels.github.io/registry/"
 $indexJson = Invoke-WebRequest -URI $repo"model-index.json"
 $index = ConvertFrom-JSON -InputObject $indexJson
-$path = $index
-$dtdlJson = Invoke-WebRequest -URI $repo$path
-$dtdl = ConvertFrom-JSON -InputObject $dtdlJson.Content
-Write-Output $dtdl
-Write-Output $dtdl
+$model = $index.'dtmi:my:device:model;1'
+$path = $repo + $model.path
+$rootJson = Invoke-WebRequest -URI $path
 
-
+$dtdl = ConvertFrom-JSON -InputObject $rootJson.Content
+Write-Output $dtdl.'@id' $path
+$model.depends | ForEach-Object { Write-Output "$model[$_].path"}
 
