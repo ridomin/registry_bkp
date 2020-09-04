9affa1f41bb27366aa9f3938878c8c2f28f879a3
ridomin <ridomin@live.com>
2020-09-04 00:38:39 -0700

    small refactors

--- .github/workflows/validate-index.js
+++ .github/workflows/validate-index.js
@@ -1,21 +1,9 @@
 const fs = require('fs')
 
-/**
- * @description "removes the dtmi prefix and the version, and replaces : with -"
- * @param {string} dtmi
- * @returns {string}
- */
-const dtmi2folder = (dtmi) => {
-  const parts = dtmi.toLowerCase().split(';')[0].split(':')
-  parts.shift()
-  return parts.join('-')
-}
-
 const main = () => {
   const index = JSON.parse(fs.readFileSync('model-index.json', 'utf-8'))
-  Object.keys(index).forEach(id=>{
+  Object.keys(index).forEach( id => {
     const path = index[id].path
-    const folder = dtmi2folder(id)
     if (!fs.existsSync(path)) {
       console.error(`ERROR: dtmi ${id} not found in ${path}`) 
       process.exit(-1)
--- resolve.ps1
+++ resolve.ps1
@@ -1,11 +1,11 @@
 $repo="https://iotmodels.github.io/registry/"
 $indexJson = Invoke-WebRequest -URI $repo"model-index.json"
 $index = ConvertFrom-JSON -InputObject $indexJson
-$path = $index.'dtmi:contoso:toaster;2'.path
+$path = $index
 $dtdlJson = Invoke-WebRequest -URI $repo$path
 $dtdl = ConvertFrom-JSON -InputObject $dtdlJson.Content
-Write-Output $dtdl.'@id'
-Write-Output $dtdl.contents
+Write-Output $dtdl
+Write-Output $dtdl
 
 
 
