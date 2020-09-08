9c0de023f7a2ad38f753d626edfcb5753eecbb0d
ridomin <ridomin@live.com>
2020-09-08 15:40:49 -0700

    add local files

--- .github/workflows/validate-index.js
+++ .github/workflows/validate-index.js
@@ -4,15 +4,18 @@ const main = () => {
   const index = JSON.parse(fs.readFileSync('model-index.json', 'utf-8'))
   Object.keys(index).forEach( id => {
     const path = index[id].path
-    if (!fs.existsSync(path)) {
-      console.error(`ERROR: dtmi ${id} not found in ${path}`) 
-      process.exit(-1)
-    }
-
-    const model = JSON.parse(fs.readFileSync(path,'utf-8'))
-    if (model['@id'] !== id ) {
-      console.error(`ERROR: File ${path} does not match expected dtmi: ${id}, but found ${model['@id']}.`)
-      process.exit(-1)
+    if (Array.isArray(path)) {
+      path.forEach(p => {
+        if (!fs.existsSync(p)) {
+          console.error(`ERROR: dtmi ${id} not found in ${p}`) 
+          process.exit(-1)
+        }    
+      })
+    } else {
+      if (!fs.existsSync(path)) {
+        console.error(`ERROR: dtmi ${id} not found in ${path}`) 
+        process.exit(-1)
+      }
     }
 
     const deps = index[id].depends || []
